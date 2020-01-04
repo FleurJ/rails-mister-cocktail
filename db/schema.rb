35727f7a1b51bdfc35e1dc14c106b4eb12b9128c
Fleur Jasoigne <fleur.jasoigne@ares-ac.be>
2020-01-04 15:48:21 +0100

    spec/models

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_04_125924) do
+ActiveRecord::Schema.define(version: 2020_01_04_135002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "coktails", force: :cascade do |t|
-    t.text "name"
+  create_table "cocktails", force: :cascade do |t|
+    t.string "name"
+    t.bigint "ingredient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_cocktails_on_ingredient_id"
   end
 
   create_table "doses", force: :cascade do |t|
-    t.text "description"
-    t.integer "ingredient_id"
-    t.integer "cocktail_id"
+    t.string "description"
+    t.bigint "ingredient_id"
+    t.bigint "cocktail_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -35,4 +39,7 @@ ActiveRecord::Schema.define(version: 2020_01_04_125924) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cocktails", "ingredients"
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
